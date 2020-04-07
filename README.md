483efcccec8e98f7df5d5cdcc2833b55a38bc12f
garretgonzales <garret915@gmail.com>
2020-04-06 21:10:17 -0500

    Updated README/Pushing Pseudo

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# fit-intermix
+# fit-intermix-mini
+
+<h4>Goal:</h4> Create app that creates a full body workout.
+<p>
+
+Trello : TBD
+<h4>Technologies:</h4>
+JavaScript, HTML5.
+
+---------------------------------
+<h4>MVP:</h4>
+
+- [ ] Produces a 5-exercise full body workout.
+
+- [ ] Each workout includes 1-2 core based movements.
+
+- [ ] User is able to mark-out completed exercise.
+
+
+
+![ToBeReplacedWithPhoto](https://media.giphy.com/media/WSDawQBr7EC9G/giphy.gif)
+
+
+<h4>Future Updates:</h4>
+
+- [ ] Deploy app!
+
+- [ ] Rep/Set tracker
+
+- [ ] Mobile Friendly
+
+- [ ] Reset button
+
+- [ ] Implement CSS Styling
+
+- [ ] Timer Function in-app
+
+- [ ] Bodyweight only option
---
+++ app.js
@@ -0,0 +1,37 @@
+/*
+WORKOUT RANDOMIZER
+
+- generates five exercises per workout
+- Full body.
+_____________________________________________
+
+- Body weight only option.
+- Timer function in app.
+
+_____________________________________________
+
+- rep/ set tracker based off timer or set button (user clicked)
+- Reset button for tracker.
+
+*/
+
+// i: array of objs
+// o: object
+
+// create 5 objects for each body area
+var arms = {};
+var back = {};
+var chest = {};
+var shoulders = {};
+var legs = {};
+
+// create workout object
+var workout = {};
+// create func to iterate through body objs
+var exercisePicker = function picker() {
+
+}
+// sort through body objs
+// grab one exercise from each body obj area
+// push exercises into workout obj
+// return  workout obj
